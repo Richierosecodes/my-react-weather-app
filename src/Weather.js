bed2df0049cf812a2e225000d79a85ece814a908
Richierosecodes <roselynolayinkaakintobi@gmail.com>
2023-10-19 07:59:09 +0100

    Added CSS to my project

--- src/Weather.js
+++ src/Weather.js
@@ -7,10 +7,10 @@ export default function Weather() {
             <form>
                 <div className="row">
                     <div className="col-9">
-                    <input type="Search" placeholder="Enter a city..."  className="form-control"/>
+                    <input type="Search" placeholder="Enter a city..."  className="form-control" autoFocus="on"/>
                     </div>
                     <div className="col-3">
-                    <input type="Submit" value="Search" className="btn btn-primary" />
+                    <input type="Submit" value="Search" className="btn btn-primary w-100" />
                     </div>
                 </div>
             </form>
@@ -19,10 +19,20 @@ export default function Weather() {
                 <li>Wednesday 07:00</li>
                 <li>Mostly Cloudy</li>
             </ul>
-            <div className="row">
+            <div className="row mt-3">
                 <div className="col-6">
-                    <img src="https://tse1.mm.bing.net/th?id=OIP.vbvM8BF6Hqri_DTesS4ZvQHaHa&pid=Api&P=0&h=220" alt="Mostly Cloudy" width="60"/>
-                    6 ℃|℉
+                    <div className="clearfix">
+                    <img src="https://tse1.mm.bing.net/th?id=OIP.vbvM8BF6Hqri_DTesS4ZvQHaHa&pid=Api&P=0&h=220" alt="Mostly Cloudy" width="60"  className="float-left"/>
+                       <div className="float-right">
+                       <span className="temperature">6</span>
+                        <span className="unit">℃|℉</span> 
+                        </div> 
+                        
+                        </div>
+                    
+                    
+                    
+                      
                 </div>
                 <div className="col-6">
                     <ul>
