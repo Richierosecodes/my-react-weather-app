a9077da981501a3f838aea23e871d7e46d4ae5d9
Richierosecodes <roselynolayinkaakintobi@gmail.com>
2023-12-09 18:46:20 +0100

    Added weather forecast for one day

---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,41 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+
+export default function WeatherForecastDay(props) {
+    function maximumTemperature() {
+        let temperature = Math.round(props.data.temp.maximum);
+        return `${temperature}°`;
+    }
+
+    function minimumTemperature() {
+        let temperature = Math.round(props.data.temp.manimum);
+        return `${temperature}°`;
+    }
+
+    function day() {
+        let date = new Date(props.data.dt * 1000);
+        let day = date.getDay();
+
+        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        return days[day];
+    }
+
+
+    return(
+        <div>
+            <div className="WeatherForecast-day">{day()} </div>
+                <WeatherIcon code = {props.data.weather[0].icon} size={36} />
+                <div className="WeatherForecast-temperatures">
+                    <span className="WeatherForecast-temperature-max">
+                    {maximumTemperature()}
+                    </span>
+                   
+                    <span className="WeatherForecast-temperature-min">
+                    {minimumTemperature()}
+                    </span>
+                   
+                </div>
+        </div>
+    );
+}  
